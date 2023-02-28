cb72400e9c5be895b8063d1b24d07e3cf2d4f7f4
chairul <chairul.amri@ebconnect.com>
2023-02-28 16:27:52 +0700

    Menggunakan 2 kondisi if

--- JavaScript Logic Pembuat Keputusan/app.js
+++ JavaScript Logic Pembuat Keputusan/app.js
@@ -1,7 +1,19 @@
-console.log("Sebelum Pembanding");
+// console.log("Sebelum Pembanding");
 
-if (1 + 1 === 2) {
-   console.log("Betul bro");
+// if (1 + 1 === 2) {
+//    console.log("Betul bro");
+// }
+
+// console.log("Setelah Pembanding");
+
+let angka = Math.random();
+angka = 0.5;
+console.log(angka);
+
+if (angka >= 0.5) {
+   console.log("Angka lebih besar dari 0.5");
 }
 
-console.log("Setelah Pembanding");
+if (angka <= 0.5) {
+   console.log("Angka lebih kecil dari 0.5");
+}
