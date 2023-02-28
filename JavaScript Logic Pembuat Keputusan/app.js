d34dbc00c951c02ff9cb4641b5d631606a39f246
chairul <chairul.amri@ebconnect.com>
2023-02-28 16:02:00 +0700

    Create new project JS

---
+++ JavaScript Logic Pembuat Keputusan/app.js
@@ -0,0 +1,9 @@
+let halu = 1 + 1;
+
+console.log(halu);
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log(1 + 1);
+halu = halu + 5;
+console.log(halu);
